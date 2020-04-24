b11fd87dd6b75c82a3c9faa64ee82682a4d1bb13
EC2 Default User <ec2-user@ip-172-31-80-131.ec2.internal>
2020-04-24 05:55:44 +0000

    add register and login function

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -9,7 +9,12 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
+                @if(Auth::check())
                 <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの作成', [], ['class'=>'nav-link']) !!}</li>
+                @else
+                <li class="nav-item">{!! link_to_route('signup.get', '新規登録', [], ['class'=>'nav-link']) !!}</li>
+                <li class="nav-item">{!! link_to_route('login', 'ログイン', [], ['class'=>'nav-link']) !!}</li>
+                @endif
             </ul>
         </div>
     </nav>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
 
+@if(Auth::check())
+
 <h1 class="mb-4">タスク一覧</h1>
 
 <div class="row">
@@ -27,4 +29,16 @@
 </div>
 
 {!! link_to_route('tasks.create', '新規タスクを作成する', [], ['class'=>'btn btn-primary']) !!}
+
+@else
+    <div class="text-center mb-4">
+        <h1>Tasklist</h1>
+    </div>
+    <div class="row">
+        <div class="col-sm-6 offset-sm-3">
+            {!! link_to_route('signup.get', '新規登録', [], ['class'=>'btn btn-primary btn-block mb-4']) !!}
+            {!! link_to_route('login', 'ログイン', [], ['class'=>'btn btn-success btn-block']) !!}
+        </div>
+    </div>
+@endif
 @endsection
